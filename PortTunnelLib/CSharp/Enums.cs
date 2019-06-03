53c41bf6430c8ff7874101eabecfb9ef45ae144b
Patrick72762 <51301901+patrick72762@users.noreply.github.com>
2019-06-03 09:43:14 +0200

    Create Enums.cs

---
+++ PortTunnelLib/CSharp/Enums.cs
@@ -0,0 +1,18 @@
+namespace PortTunnelLib
+{
+    public class Enums
+    {
+        public enum LogType
+        {
+            Info,
+            Warning,
+            Error
+        }
+
+        public enum ConnectionType
+        {
+            Server,
+            Client
+        }
+    }
+}
